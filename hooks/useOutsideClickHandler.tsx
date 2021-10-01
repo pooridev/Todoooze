94813e835a1aca79daf54739c744ce853076ac23
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-10-01 16:13:56 +0330

    [feat] add a hook to handle the click outside of the element

---
+++ hooks/useOutsideClickHandler.tsx
@@ -0,0 +1,50 @@
+import { useEffect, MouseEvent, RefObject } from 'react';
+
+interface IProps {
+  ref: RefObject<HTMLElement> | null;
+  secondRef: RefObject<HTMLElement> | undefined;
+  callback: () => void;
+}
+
+/**
+ * 
+ * @description This hook is used to handle the click outside of the element
+ */
+
+const useOutsideClickHandler = (props: IProps): void => {
+  const { ref, secondRef, callback } = props;
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      // In case if we want to pass the optional second ref
+      if (secondRef) {
+        if (
+          !ref?.current?.contains(event.target as Node) &&
+          !secondRef.current?.contains(event.target as Node)
+        ) {
+          event.stopPropagation();
+          callback();
+          return;
+        }
+      }
+
+      // In case if we do not want to pass the optional second ref
+      if (
+        (ref?.current && !ref.current.contains(event.target as Node)) ||
+        ref?.current?.contains(event.target as Node)
+      ) {
+        event.stopPropagation();
+        callback();
+        return;
+      }
+    };
+
+    document.addEventListener('mousedown', () => handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', () => handleClickOutside);
+    };
+  });
+};
+
+export default useOutsideClickHandler;
