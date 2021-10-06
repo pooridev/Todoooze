853341252156c279b4694a194e338a336b208123
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-10-07 01:09:26 +0330

    [feat] implement next-redux-wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,19 @@
-import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 
 import '../styles/globals.css';
 import Layout from '../layout/Layout';
-import { store } from '../redux/store';
 import { SidebarProvider } from '../providers/Sidebar';
+import { wrapper } from './../redux/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <SidebarProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SidebarProvider>
-    </Provider>
+    <SidebarProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SidebarProvider>
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,16 @@
-import { compose, createStore } from 'redux';
+import { createWrapper } from 'next-redux-wrapper';
+import { createStore, compose } from 'redux';
 import projectReducer from './reducers/project';
 
 const rootReducer = projectReducer;
 
-let composeEnhancers = compose;
-if (typeof window !== 'undefined') {
-  composeEnhancers = window?.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-}
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
 
+const enhancer = composeEnhancers();
 
-export const store = createStore(rootReducer, composeEnhancers());
+const makeStore = () => createStore(rootReducer, enhancer);
+
+export const wrapper = createWrapper(makeStore);
