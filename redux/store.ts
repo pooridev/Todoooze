567f4d644bf35219a53091b4ed71391a971dd91b
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-10-06 23:32:35 +0330

    [fix] resolve linter warning

--- redux/store.ts
+++ redux/store.ts
@@ -3,9 +3,10 @@ import projectReducer from './reducers/project';
 
 const rootReducer = projectReducer;
 
-const enhancers = compose(
-  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__
-    ? window.devToolsExtension()
-    : f => f
-);
-export const store = createStore(rootReducer, enhancers);
+let composeEnhancers = compose;
+if (typeof window !== 'undefined') {
+  composeEnhancers = window?.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
+
+export const store = createStore(rootReducer, composeEnhancers());
