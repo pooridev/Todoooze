6c147f91213099a3de68418243371c8d8cad050f
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-10-14 16:39:16 +0330

    [format] reformat usePatch hook

--- hooks/usePatch.tsx
+++ hooks/usePatch.tsx
@@ -1,9 +1,10 @@
-import {useState} from "react";
+import { useState } from 'react';
 
-export const usePatch = (initialSchema) => {
-	const [data, setData] = useState(initialSchema);
+export const usePatch = initialSchema => {
+  const [data, setData] = useState(initialSchema);
 
-	const setField = (field_item) => setData(prevState => ({...prevState, ...field_item}))
-	
+  const setField = field_item =>
+    setData(prevState => ({ ...prevState, ...field_item }));
+
   return [data, setField, setData];
 };
