eedb471ec4664eb1d3f77bedf5fea2f36654c911
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-10-23 22:52:31 +0330

    [update] use dot notation for using css classes

--- components/Sidebar/Projects/index.tsx
+++ components/Sidebar/Projects/index.tsx
@@ -8,66 +8,73 @@ import { ArrowRight, ThreeDotsIcon } from '../../../shared/icon';
 import { IProjectState } from '../../../types/IProjectState';
 import { ProjectType } from '../../../types/ProjectType';
 import { TaskType } from '../../../types/TaskType';
+import { removeFalseys } from '../../../helpers/strings';
 
 /**
  *
  * @description a component that renders a list of projects on the main sidebar.
  */
 
-const Lists: FC = () => {
-  const [areListsOpen, setAreListsOpen] = useState(false);
+const Projects: FC = () => {
+  const [areProjectsOpen, setAreProjectsOpen] = useState(false);
   const { projects } = useSelector((state: IProjectState) => state);
 
   const dispatch = useDispatch();
 
-  const toggleListsDropdown = () => setAreListsOpen(prev => !prev);
+  const toggleListsDropdown = () => setAreProjectsOpen(prev => !prev);
 
   return (
-    <div className={styles['Lists']}>
-      <header className={styles['Header']} onClick={toggleListsDropdown}>
+    <div className={styles.Lists}>
+      <header className={styles.Header} onClick={toggleListsDropdown}>
         <h3>
           Your projects
-          <ArrowRight className={areListsOpen ? styles['RotateArrow'] : ''} />
+          <ArrowRight
+            className={removeFalseys(areProjectsOpen && styles.RotateArrow)}
+          />
         </h3>
       </header>
       <ul
-        className={`${styles['Projects']}${
-          areListsOpen ? styles['Open'] : ''
-        }`}>
+        className={removeFalseys(
+          styles.Projects,
+          areProjectsOpen && styles.Open
+        )}>
         {projects.map((project: ProjectType) => (
-          <li key={project.id} className={styles['Project']}>
+          <li key={project.id} className={styles.Project}>
             <div
               key={project.id}
-              className={styles['Title']}
-              onClick={() => dispatch(openList(project['id']))}>
+              className={styles.Title}
+              onClick={() => dispatch(openList(project.id))}>
               <span className='flex items-center gap-2 w-full'>
                 <ArrowRight
-                  className={project['isOpen'] ? styles['RotateArrow'] : ''}
+                  className={removeFalseys(
+                    project.isOpen && styles.RotateArrow
+                  )}
                 />
-                <span>{project['title']}</span>
+                <span>{project.title}</span>
               </span>
-              <button className={styles['OpenOptionsButton']}>
+              <button className={styles.OpenOptionsButton}>
                 <ThreeDotsIcon />
               </button>
             </div>
             <ul
-              className={`${styles['Tasks']} ${
-                project['isOpen'] ? styles['OpenTasks'] : ''
-              }`}>
-              {project['tasks']
+              className={removeFalseys(
+                styles.Tasks,
+                project.isOpen && styles.OpenTasks
+              )}>
+              {project.tasks
                 .filter(task => task.status === 'todo')
                 .slice(0, 3)
                 .map((task: TaskType) => (
-                  <li className={styles['Task']} key={task['id']}>
+                  <li className={styles.Task} key={task.id}>
                     <input
                       type='checkbox'
-                      id={task['title']}
-                      className={styles['TaskCheckbox']}
+                      id={task.title}
+                      className={styles.TaskCheckbox}
                     />
-                    <label htmlFor={task['title']}>{task['title']}</label>
+                    <label htmlFor={task.title}>{task.title}</label>
                   </li>
                 ))}
-              <li className={styles['Task']}>
+              <li className={styles.Task}>
                 <Link href={`/project/${project.id}`}>Go To Project</Link>
               </li>
             </ul>
@@ -78,4 +85,4 @@ const Lists: FC = () => {
   );
 };
 
-export default Lists;
+export default Projects;
