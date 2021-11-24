1f56f28ae4b9f721ea6d5b53a66e13d1e5953e58
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-11-24 23:25:32 +0330

    [update] delete icons folder (TEMPORARY)

--- components/shared/icon/ArrowRightIcon.tsx
+++
@@ -1,9 +0,0 @@
-import { SVGProps } from 'react';
-
-const ArrowRight = (props: SVGProps<SVGSVGElement>) => (
-  <svg {...props} width='6' height='7' viewBox='0 0 6 7' fill='#8A8F98'>
-    <path d='M5.25 3.20126C5.58333 3.39371 5.58333 3.87484 5.25 4.06729L0.749999 6.66537C0.416666 6.85782 -2.38418e-07 6.61725 -2.38418e-07 6.23235L-2.38418e-07 1.0362C-2.38418e-07 0.651301 0.416666 0.410738 0.75 0.603188L5.25 3.20126Z'></path>
-  </svg>
-);
-
-export { ArrowRight };
--- components/shared/icon/ThreeDotsIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React, { SVGProps } from 'react';
-
-const ThreeDotsIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg {...props} width='14' height='14' viewBox='0 0 14 14' fill='#8A8F98'>
-    <path d='M10 7C10 7.825 10.675 8.5 11.5 8.5C12.325 8.5 13 7.825 13 7C13 6.175 12.325 5.5 11.5 5.5C10.675 5.5 10 6.175 10 7ZM8.5 7C8.5 6.175 7.825 5.5 7 5.5C6.175 5.5 5.5 6.175 5.5 7C5.5 7.825 6.175 8.5 7 8.5C7.825 8.5 8.5 7.825 8.5 7ZM4 7C4 6.175 3.325 5.5 2.5 5.5C1.675 5.5 1 6.175 1 7C1 7.825 1.675 8.5 2.5 8.5C3.325 8.5 4 7.825 4 7Z'></path>
-  </svg>
-);
-
-export { ThreeDotsIcon };
