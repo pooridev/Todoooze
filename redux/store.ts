ead7dba7f87918ad04d3b3ea55ab0b82c7be8777
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2022-07-05 00:08:18 +0430

    refactor: set global namespace for Redux dev tools

--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,17 @@
 import { createStore, compose } from 'redux';
 import projectReducer from './reducers/project';
 
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
 const rootReducer = projectReducer;
 
 let composeEnhancers = compose;
 
 if (typeof window !== 'undefined') {
-  // @ts-ignore
   composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 }
 export const store = createStore(rootReducer, composeEnhancers());
